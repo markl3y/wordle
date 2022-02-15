8d6ba38fa5bfb5babf6304037a2b4239a9067727
Tyler Bosh <tylermarkleybosh@gmail.com>
2022-02-15 18:05:20 -0500

    Completed exercises for introduction to SQL

--- module-2/01_Intro_to_Database/exercise/Exercises/19_population_density_all.sql
+++ module-2/01_Intro_to_Database/exercise/Exercises/19_population_density_all.sql
@@ -1,3 +1,4 @@
 -- 19. The name, population, area, and population density (name the column 'population_density') of all states, territories, and districts (56 rows)
 -- Population density is expressed as people per square kilometer. In other words, population divided by area.
-
+SELECT state_name, population, area, cast (cast(population as decimal(10,2))/cast(area as decimal(10,2)) as decimal(10,2)) AS population_density 
+FROM state
--- module-2/01_Intro_to_Database/exercise/Exercises/20_population_density_high.sql
+++ module-2/01_Intro_to_Database/exercise/Exercises/20_population_density_high.sql
@@ -1,3 +1,5 @@
 -- 20. The name, population, area, and population density (name the column 'population_density') of states, territories, and districts with more than 100 people per square kilometer (12 rows)
 -- Population density is expressed as people per square kilometer. In other words, population divided by area.
-
+SELECT state_name, population, area, cast(cast(population as decimal(10,2))/cast(area as decimal(10,2)) as decimal(10,2)) AS population_density 
+FROM state
+WHERE cast(population as decimal(10,2))/cast(area as decimal(10,2)) > 100
