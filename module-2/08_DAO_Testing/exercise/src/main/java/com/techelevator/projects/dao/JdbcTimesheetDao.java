22a42d5df7874973d48ed24c229b12f1c055945f
Tyler Bosh <tylermarkleybosh@gmail.com>
2022-02-24 15:51:57 -0500

    Completed exercises for DAO testing.

--- module-2/08_DAO_Testing/exercise/src/main/java/com/techelevator/projects/dao/JdbcTimesheetDao.java
+++ module-2/08_DAO_Testing/exercise/src/main/java/com/techelevator/projects/dao/JdbcTimesheetDao.java
@@ -21,7 +21,7 @@ public class JdbcTimesheetDao implements TimesheetDao {
         Timesheet timesheet = null;
         String sql = "SELECT timesheet_id, employee_id, project_id, date_worked, hours_worked, billable, description " +
                      "FROM timesheet " +
-                     "WHERE timesheet_id = ?";
+                     "WHERE timesheet_id = ?;";
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql, timesheetId);
         if (results.next()) {
             timesheet = mapRowToTimesheet(results);
@@ -37,7 +37,7 @@ public class JdbcTimesheetDao implements TimesheetDao {
                      "WHERE employee_id = ? " +
                      "ORDER BY timesheet_id;";
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql, employeeId);
-        if (results.next()) {
+        while (results.next()) {
             Timesheet timesheet = mapRowToTimesheet(results);
             timesheets.add(timesheet);
         }
@@ -49,7 +49,7 @@ public class JdbcTimesheetDao implements TimesheetDao {
         List<Timesheet> timesheets = new ArrayList<>();
         String sql = "SELECT timesheet_id, employee_id, project_id, date_worked, hours_worked, billable, description " +
                      "FROM timesheet " +
-                     "WHERE employee_id = ? " +
+                     "WHERE project_id = ? " +
                      "ORDER BY timesheet_id;";
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql, projectId);
         while (results.next()) {
@@ -69,19 +69,19 @@ public class JdbcTimesheetDao implements TimesheetDao {
         return getTimesheet(newId);
     }
 
-    @Override
+    @Override //Here's the bug, it doesn't update billable
     public void updateTimesheet(Timesheet updatedTimesheet) {
         String sql = "UPDATE timesheet " +
-                     "SET employee_id = ?, project_id = ?, date_worked = ?, hours_worked = ?, description = ? " +
-                     "WHERE timesheet_id = ?";
+                     "SET employee_id = ?, project_id = ?, date_worked = ?, hours_worked = ?, billable = ?, description = ? " +
+                     "WHERE timesheet_id = ?;";
         jdbcTemplate.update(sql, updatedTimesheet.getEmployeeId(), updatedTimesheet.getProjectId(),
-                updatedTimesheet.getDateWorked(), updatedTimesheet.getHoursWorked(), updatedTimesheet.getDescription(),
+                updatedTimesheet.getDateWorked(), updatedTimesheet.getHoursWorked(), updatedTimesheet.isBillable(), updatedTimesheet.getDescription(),
                 updatedTimesheet.getTimesheetId());
     }
 
     @Override
     public void deleteTimesheet(Long timesheetId) {
-        String sql = "DELETE FROM timesheet WHERE timesheet_id = ?";
+        String sql = "DELETE FROM timesheet WHERE timesheet_id = ?;";
         jdbcTemplate.update(sql, timesheetId);
     }
 
@@ -90,7 +90,7 @@ public class JdbcTimesheetDao implements TimesheetDao {
         double billableHours = 0;
         String sql = "SELECT SUM(hours_worked) AS billable_hours " +
                      "FROM timesheet " +
-                     "WHERE employee_id = ? AND project_id = ?";
+                     "WHERE employee_id = ? AND project_id = ? AND billable = true;";
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql, employeeId, projectId);
         if (results.next()) {
             billableHours = results.getDouble("billable_hours");
--- module-2/08_DAO_Testing/exercise/src/test/java/com/techelevator/projects/dao/JdbcTimesheetDaoTests.java
+++ module-2/08_DAO_Testing/exercise/src/test/java/com/techelevator/projects/dao/JdbcTimesheetDaoTests.java
@@ -20,66 +20,130 @@ public class JdbcTimesheetDaoTests extends BaseDaoTests {
             LocalDate.parse("2021-02-01"), 2.0, false, "Timesheet 4");
     
     private JdbcTimesheetDao sut;
-
+    private Timesheet testTimesheet;
 
     @Before
     public void setup() {
         sut = new JdbcTimesheetDao(dataSource);
+        testTimesheet = new Timesheet(99L, 1L, 2L, LocalDate.now(), 4.0, true, "Test Timesheet");
     }
 
     @Test
     public void getTimesheet_returns_correct_timesheet_for_id() {
-        Assert.fail();
+        Timesheet timesheet = sut.getTimesheet(4L);
+        assertTimesheetsMatch("getTimesheet returns incorrect timesheet for timesheet id", TIMESHEET_4, timesheet);
     }
 
     @Test
     public void getTimesheet_returns_null_when_id_not_found() {
-        Assert.fail();
+        Timesheet timesheet = sut.getTimesheet(8L);
+        Assert.assertNull("getTimesheet did not return null when id not found", timesheet);
     }
 
     @Test
     public void getTimesheetsByEmployeeId_returns_list_of_all_timesheets_for_employee() {
-        Assert.fail();
+        List<Timesheet> timesheets = sut.getTimesheetsByEmployeeId(1L);
+        Assert.assertEquals("getTimesheetsByEmployeeId returned wrong number of timesheets", 2, timesheets.size());
+
+        timesheets = sut.getTimesheetsByEmployeeId(3L);
+        Assert.assertEquals("getTimesheetsByEmployeeId returned timesheet(s) for employee id not in database", 0, timesheets.size());
     }
 
     @Test
     public void getTimesheetsByProjectId_returns_list_of_all_timesheets_for_project() {
-        Assert.fail();
+        List<Timesheet> timesheets = sut.getTimesheetsByProjectId(1L);
+        Assert.assertEquals("getTimesheetsByProjectId returned wrong number of timesheets", 3, timesheets.size());
+
+        timesheets = sut.getTimesheetsByProjectId(3L);
+        Assert.assertEquals("getTimesheetsByProjectId returned timesheet(s) for project id not in database", 0, timesheets.size());
     }
 
     @Test
     public void createTimesheet_returns_timesheet_with_id_and_expected_values() {
-        Assert.fail();
+        Timesheet createdTimesheet = sut.createTimesheet(testTimesheet);
+
+        Assert.assertNotNull("createTimesheet returned null", createdTimesheet);
+
+        long newId = createdTimesheet.getTimesheetId();
+        Assert.assertTrue("createTimesheet failed to return a timesheet with an id", newId > 0);
+
+        testTimesheet.setTimesheetId(newId);
+        assertTimesheetsMatch("createTimesheet did not return timesheet with id and expected values", testTimesheet, createdTimesheet);
     }
 
     @Test
     public void created_timesheet_has_expected_values_when_retrieved() {
-        Assert.fail();
+        Timesheet createdTimesheet = sut.createTimesheet(testTimesheet);
+
+        Assert.assertNotNull("createTimesheet must work before tests can be done to retrieve correct values", createdTimesheet);
+
+        long newId = createdTimesheet.getTimesheetId();
+        Timesheet retrievedTimesheet = sut.getTimesheet(newId);
+
+        assertTimesheetsMatch("createTimesheet did not create timesheet",createdTimesheet, retrievedTimesheet);
     }
 
     @Test
     public void updated_timesheet_has_expected_values_when_retrieved() {
-        Assert.fail();
+        Timesheet timesheet = sut.getTimesheet(1L);
+
+        Assert.assertNotNull("getTimesheet must work before tests can be done to retrieve correct values from updateTimesheet", timesheet);
+
+        timesheet.setEmployeeId(2L);
+        sut.updateTimesheet(timesheet);
+        Timesheet updatedTimesheet = sut.getTimesheet(1L);
+        Assert.assertEquals("updateTimesheet did not update employee id in database", Long.valueOf(2), updatedTimesheet.getEmployeeId());
+
+        timesheet.setProjectId(2L);
+        sut.updateTimesheet(timesheet);
+        updatedTimesheet = sut.getTimesheet(1L);
+        Assert.assertEquals("updateTimesheet did not update project id in database", Long.valueOf(2), updatedTimesheet.getProjectId());
+
+        timesheet.setDateWorked(LocalDate.parse("2010-01-10"));
+        sut.updateTimesheet(timesheet);
+        updatedTimesheet = sut.getTimesheet(1L);
+        Assert.assertEquals("updateTimesheet did not update date worked in database", LocalDate.parse("2010-01-10"), updatedTimesheet.getDateWorked());
+
+        timesheet.setHoursWorked(3.0);
+        sut.updateTimesheet(timesheet);
+        updatedTimesheet = sut.getTimesheet(1L);
+        Assert.assertEquals("updateTimesheet did not update hours worked in database", Double.valueOf(3.0), Double.valueOf(updatedTimesheet.getHoursWorked()));
+
+        timesheet.setBillable(!timesheet.isBillable());
+        sut.updateTimesheet(timesheet);
+        updatedTimesheet = sut.getTimesheet(1L);
+        Assert.assertFalse("updateTimesheet did not update is billable in database", updatedTimesheet.isBillable());
+
+        timesheet.setDescription("Test Timesheet");
+        sut.updateTimesheet(timesheet);
+        updatedTimesheet = sut.getTimesheet(1L);
+        Assert.assertEquals("updateTimesheet did not update description in database", "Test Timesheet", updatedTimesheet.getDescription());
     }
 
     @Test
     public void deleted_timesheet_cant_be_retrieved() {
-        Assert.fail();
+        sut.deleteTimesheet(1L);
+
+        Timesheet timesheet = sut.getTimesheet(1L);
+        Assert.assertNull("deleteTimesheet did not remove timesheet from database", timesheet);
     }
 
     @Test
     public void getBillableHours_returns_correct_total() {
-        Assert.fail();
+        double billableHours = sut.getBillableHours(1L,1L);
+        Assert.assertEquals("getBillableHours did not return correct total for employee id and project id", Double.valueOf(2.5), Double.valueOf(billableHours));
+
+        billableHours = sut.getBillableHours(2L,2L);
+        Assert.assertEquals("getBillableHours did not return correct total for employee id and project id", Double.valueOf(0), Double.valueOf(billableHours));
     }
 
-    private void assertTimesheetsMatch(Timesheet expected, Timesheet actual) {
-        Assert.assertEquals(expected.getTimesheetId(), actual.getTimesheetId());
-        Assert.assertEquals(expected.getEmployeeId(), actual.getEmployeeId());
-        Assert.assertEquals(expected.getProjectId(), actual.getProjectId());
-        Assert.assertEquals(expected.getDateWorked(), actual.getDateWorked());
-        Assert.assertEquals(expected.getHoursWorked(), actual.getHoursWorked(), 0.001);
-        Assert.assertEquals(expected.isBillable(), actual.isBillable());
-        Assert.assertEquals(expected.getDescription(), actual.getDescription());
+    private void assertTimesheetsMatch(String message, Timesheet expected, Timesheet actual) {
+        Assert.assertEquals(message, expected.getTimesheetId(), actual.getTimesheetId());
+        Assert.assertEquals(message, expected.getEmployeeId(), actual.getEmployeeId());
+        Assert.assertEquals(message, expected.getProjectId(), actual.getProjectId());
+        Assert.assertEquals(message, expected.getDateWorked(), actual.getDateWorked());
+        Assert.assertEquals(message, expected.getHoursWorked(), actual.getHoursWorked(), 0.001);
+        Assert.assertEquals(message, expected.isBillable(), actual.isBillable());
+        Assert.assertEquals(message, expected.getDescription(), actual.getDescription());
     }
-
 }
